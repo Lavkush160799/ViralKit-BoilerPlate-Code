feebf68cce1d9e38dc14e2de153c4e3753db837a
luvkushbentsys <luvkush.yadav@bentsys.com>
2026-03-11 00:00:39 +0530

    updated

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView className="flex-1 items-center justify-center " edges={[""]}> 
+    <SafeAreaView className="flex-1 items-center justify-center " edges={['top']}> 
       <StatusBar barStyle="dark-content"/>
       {/* Header */}
       <Text className="text-2xl font-bold">index</Text>
